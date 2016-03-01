346f2ed511bba4eb21c3284d619e69044050d81f
yahyaermaya <yahya2142@gmail.com>
2016-03-02 00:31:44 +0700

    nambahin atribut berkas sama lowongan

--- TUBES_PBO/src/tubes_pbo/Pelamar.java
+++ TUBES_PBO/src/tubes_pbo/Pelamar.java
@@ -1,7 +1,17 @@
 package tubes_pbo;
 
 public class Pelamar extends Orang {
+    private Berkas[] berkas;
+    
     public Pelamar(String nama) {
         super(nama);
+    }
+    
+    public void setBerkas(Berkas berkas) {
+        this.berkas = berkas;
+    }
+    
+    public Berkas getBerkas() {
+        return berkas;
     }
 }
--- TUBES_PBO/src/tubes_pbo/Perusahaan.java
+++ TUBES_PBO/src/tubes_pbo/Perusahaan.java
@@ -1,7 +1,17 @@
 package tubes_pbo;
 
 public class Perusahaan extends Orang {
+    private Lowongan[] lowongan;
+    
     public Perusahaan(String nama) {
         super(nama);
+    }
+    
+    public void setLowongan(Lowongan lowongan) {
+        this.berkas = berkas;
+    }
+    
+    public Lowongan getLowongan() {
+        return lowongan;
     }
 }
