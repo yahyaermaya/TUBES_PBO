33dc5c7a55f13c842723fd7bb0288d7078088fae
yahyaermaya <yahya2142@gmail.com>
2016-04-26 21:15:19 +0700

    Banyak yang diupdate dan tambahan beberapa class baru

--- TUBES_PBO/src/tubes_pbo/Driver.java
+++ TUBES_PBO/src/tubes_pbo/Driver.java
@@ -4,7 +4,6 @@ import Controller.ControllerMainMenu;
 
 public class Driver {
     public static void main(String[] args) {
-        Aplikasi aplikasi = new Aplikasi();
         Aplikasi app= new Aplikasi();
         ControllerMainMenu cmm = new ControllerMainMenu(app);
     }
